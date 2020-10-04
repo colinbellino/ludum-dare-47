ae3d5a9817a0f9ee0b463ea0e124c8e8fec6a76f
Erehuc <huc.romain@gmail.com>
2020-10-04 16:54:42 +0200

    Display game over screen

--- Assets/Code/GameJam.Core/GameConfig.cs
+++ Assets/Code/GameJam.Core/GameConfig.cs
@@ -5,6 +5,7 @@ public class GameConfig : ScriptableObject
 {
 	public string TitleSceneName = "Title";
 	public string MainSceneName = "Main";
+	public string GameOverSceneName = "GameOver";
 	public string GroundTag = "Ground";
 	public LayerMask GroundLayer;
 	public LayerMask InteractiveLayer;
--- Assets/Code/GameJam.Core/GameStateMachine/GameOverState.cs
+++ Assets/Code/GameJam.Core/GameStateMachine/GameOverState.cs
@@ -1,20 +1,23 @@
-using System.Linq;
-using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
 using Zenject;
 
 public class GameOverState : IState
 {
 	private readonly GameStateMachine _machine;
 	private readonly GameState _gameState;
+	private readonly GameConfig _gameConfig;
 
-	public GameOverState(GameStateMachine machine, GameState gameState)
+	public GameOverState(GameStateMachine machine, GameState gameState, GameConfig gameConfig)
 	{
 		_machine = machine;
+		_gameConfig = gameConfig;
 	}
 
-	public void Enter(object[] parameters)
+	public async void Enter(object[] parameters)
 	{
 		UnityEngine.Debug.Log("game over");
+		await SceneManager.LoadSceneAsync(_gameConfig.GameOverSceneName);
 	}
 
 	public void Tick() { }
