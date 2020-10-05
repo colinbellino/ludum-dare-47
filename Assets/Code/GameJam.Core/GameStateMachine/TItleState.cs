8376bbac7f309289cca79803586e49e10afa2ce3
Colin Bellino <bellino.colin@gmail.com>
2020-10-05 21:23:56 +0200

    ✨ Title sceen <3

--- Assets/Code/GameJam.Core/GameStateMachine/TItleState.cs
+++ Assets/Code/GameJam.Core/GameStateMachine/TItleState.cs
@@ -1,6 +1,19 @@
 
+using DG.Tweening;
+using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using Zenject;
+using Cysharp.Threading.Tasks;
+
+public class StaticKillDOTween
+{
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+	static void Init()
+	{
+		DOTween.KillAll();
+	}
+}
 
 public class TitleState : IState
 {
@@ -13,15 +26,35 @@ public class TitleState : IState
 		_gameConfig = gameConfig;
 	}
 
-	public void Enter(object[] parameters)
+	public async void Enter(object[] parameters)
 	{
 		GameEvents.QuitGame += QuitGame;
 		GameEvents.StartGame += StartGame;
+
+		var startImage = GameObject.Find("Press Start").GetComponent<Image>();
+		startImage.color = Color.clear;
+
+		await UniTask.Delay(1000);
+
+		await startImage.DOColor(Color.white, duration: 1f).AsyncWaitForCompletion();
+
+		await UniTask.Delay(4000);
+
+		startImage.DOColor(_gameConfig.Color4, duration: 1f).SetLoops(50, LoopType.Yoyo);
 	}
 
 	public void Tick()
 	{
-		if (Keyboard.current.enterKey.wasPressedThisFrame)
+		if (Keyboard.current.escapeKey.wasPressedThisFrame)
+		{
+			QuitGame();
+		}
+		else if (Mouse.current.leftButton.wasPressedThisFrame)
+		{
+			StartGame();
+
+		}
+		else if (Keyboard.current.anyKey.wasPressedThisFrame)
 		{
 			StartGame();
 		}
